664f7fd5d227e21ec9ab963d69b9cccdb34cff85
Nitai j. Perez <nitai.perez@ironsrc.com>
2015-07-16 04:04:44 +0300

    Implemented Multiline popup view.

--- src/browser_action/browser_action.js
+++ src/browser_action/browser_action.js
@@ -20,11 +20,31 @@ chrome.runtime.sendMessage({getPRdiv: true}, function (response) {
 
     // Fill in content:
     document.getElementById('body').innerHTML = (localStorage.div || '<div style="padding: 20px;">Loading...</div>');
-    var remove = ['.spinner', '.source', '.destination', '.aui-avatar-project'];
+    var remove = ['.spinner', '.aui-avatar-project'];//, '.source', '.destination'];
     remove.forEach(function (selector) {
         $(selector).each(function () {
             this.remove()
         })
     });
     $("a").attr("target", "_blank");
+    // Break rows:
+
+    $('tr').each(function () {
+        var newTR = $('<tr>');
+        $.each(this.attributes, function() {
+            newTR.attr(this.name, this.value);
+        });
+        //var cToMove = [1,5,5,5];
+        var cToMove = [6,6,6];
+
+        var oldTR = this;
+        cToMove.forEach(function (i) {
+            newTR.insertAfter(oldTR).append($('th:eq(' + i + '), td:eq(' + i + ')', oldTR));
+        })
+    });
+    //$('.title').attr('colspan',2);
+    $('.source').attr('colspan',2);
+    $('.destination').attr('colspan',2);
+    $('.updated').attr('colspan',2);
+
 });
