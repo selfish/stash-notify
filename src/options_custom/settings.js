dfcf9fa56dcaf872de728a13aafa3b2247cb14ef
Nitai j. Perez <nitai.perez@ironsrc.com>
2015-07-16 17:27:07 +0300

    Removed erroneous code.

--- src/options_custom/settings.js
+++ src/options_custom/settings.js
@@ -1,8 +1,8 @@
 window.addEvent("domready", function () {
     // Option 1: Use the manifest:
     new FancySettings.initWithManifest(function (settings) {
-        settings.manifest.myButton.addEvent("action", function () {
-            alert("You clicked me!");
-        });
+        //settings.manifest.myButton.addEvent("action", function () {
+        //    alert("You clicked me!");
+        //});
     });
 });
