a5182516944b349aae62f1e0dd70de882f41b28f
Nitai J. Perez <Nitai.Perez@Gmail.com>
2016-05-12 03:53:32 +0300

    WIP - Complete rewrite to angularJS.

---
+++ src/services/stash.service.js
@@ -0,0 +1,172 @@
+/**
+ * Created by Nitai J. Perez
+ * nitai.perez@ironsrc.com
+ * on 10/05/2016
+ */
+
+app.factory('stash', ['$localStorage', 'util', 'badge', ($ls, util, badge) => {
+
+    function _log(str) {
+        console.log(`%c ST: ${str}`, "background-color: #DCE2FF;");
+    }
+
+    const urls = {
+        pullRequestsURL: '/rest/inbox/latest/pull-requests?role=reviewer&start=0&limit=10&avatarSize=64&state=OPEN&order=oldest',
+        myPullRequestsURL: '/rest/inbox/latest/pull-requests?role=author&start=0&limit=10&avatarSize=64&state=OPEN&order=oldest',
+        whoAmI: '/plugins/servlet/applinks/whoami'
+    };
+
+    function prFetch() {
+        $ls.loading = true;
+        _log('Fetching PR Data');
+        return util.get(urls.pullRequestsURL)
+            .then(_prPrepare)
+            .then(_getTasks)
+            .then(_getMergeStatus)
+            .then((res) => {
+                if (res.size == 0) badge.clear();
+                else badge.set(res.size, null, "#ff0000");
+                $ls.prData = res;
+                _log('--- Success.');
+                $ls.error = false;
+                return res;
+            })
+            .catch((e)=> {
+                _log(`--- Failure: ${e}`);
+                if (!$ls.prData) {
+                    $ls.error = true;
+                }
+            }).finally(()=> {
+                _log('--- Load complete ---');
+                $ls.loading = false;
+            });
+    }
+
+    function prFetchMine() {
+        $ls.loading = true;
+        _log('Fetching MY PR Data');
+        util.get(urls.myPullRequestsURL)
+            .then(_prPrepare)
+            .then(_getTasks)
+            .then(_getMergeStatus)
+            .then((res) => {
+                $ls.prDataMine = res;
+                _log('--- Success.');
+                $ls.error = false;
+                return res;
+            })
+            .catch((e)=> {
+                _log(`--- Failure: ${e}`);
+            }).finally(()=> {
+            _log('--- Load complete ---');
+            $ls.loading = false;
+        });
+    }
+
+// previously filterResult
+    function _prPrepare(data) {
+        _log(`> Prepare data (${data.values.length} items)`);
+        // Open tasks:
+        if ($ls.config.hide_pr_with_tasks) {
+            data.values = data.values.filter((pr) => {
+                return _.get(pr, 'properties.openTaskCount') == 0;
+            });
+        }
+
+        // Scrum master:
+        if ($ls.config.scrum_master && _me()) {
+
+            data.values = data.values.filter((pr) => {
+                // If I'm the only reviewer, display:
+                if (pr.reviewers.length == 1) return true;
+                // Else, if at least one other had reviewed:
+                return _.some(pr["reviewers"], (rev) => {
+                    return (rev.approved == true && rev.user.name != _me());
+                });
+            });
+        }
+
+        data.size = data.values.length;
+        //return data;
+        return data;
+    }
+
+    function _me() {
+        if (!_.isUndefined($ls.me)) return $ls.me;
+        else {
+            // Will return null, and skip the check.
+            util.get(urls.whoAmI)
+                .then((data) => {
+                    $ls.me = data;
+                    _log(`me fetched: '${$ls.me}'`);
+                }).catch(_.noop);
+        }
+    }
+
+    function _getTasksByPR(pr, limit) {
+        const stashRemote = util.stashRemoteFromUrl(pr.links.self[0].href);
+        const url = [
+            stashRemote.scheme, '://',
+            stashRemote.host, '/rest/api/1.0',
+            '/projects/', stashRemote.project,
+            '/repos/', stashRemote.repo,
+            '/pull-requests/', stashRemote.pr,
+            '/tasks?start=0&limit=', limit || 5
+        ].join('');
+
+        return util.get(url).then((taskData) => {
+            return taskData.values.map((t) => {
+                return {text: t.text, author: t.author.displayName, state: t.state, created: t.createdDate}
+            });
+        });
+    }
+
+    function _getTasks(data) {
+        _log(`> Getting tasks data: (${data.values.length} items)`);
+        return Promise.map(data.values, (pr) => {
+                return _getTasksByPR(pr)
+                    .then((tasks) => {
+                        pr.tasks = tasks;
+                        return pr;
+                    });
+            })
+            .then((res) => {
+                data.values = res;
+                return data;
+            });
+    }
+
+    function _getMergeStatusByPR(pr) {
+        const stashRemote = util.stashRemoteFromUrl(pr.links.self[0].href);
+        const url = [
+            $ls.stashURL, '/rest/api/1.0',
+            '/projects/', stashRemote.project,
+            '/repos/', stashRemote.repo,
+            '/pull-requests/', stashRemote.pr,
+            '/merge'
+        ].join('');
+        return util.get(url);
+    }
+
+    function _getMergeStatus(data) {
+        _log(`> Getting Merge Status: (${data.values.length} items)`);
+        return Promise.map(data.values, (pr) => {
+                return _getMergeStatusByPR(pr)
+                    .then((mergeStatus) => {
+                        pr.mergeStatus = mergeStatus;
+                        return pr;
+                    });
+            })
+            .then((res) => {
+                data.values = res;
+                return data;
+            });
+    }
+
+
+    return {
+        prFetch: prFetch,
+        prFetchMine: prFetchMine
+    }
+
+}]);
---
+++ src/services/util.service.js
@@ -0,0 +1,166 @@
+/**
+ * Created by Nitai J. Perez
+ * nitai.perez@ironsrc.com
+ * on 09/05/2016
+ */
+
+app.factory('util', ['$localStorage', '$http', ($ls, $http) => {
+
+
+    function uuid() {
+        return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, a=>(a ^ Math.random() * 16 >> a / 4).toString(16));
+    }
+
+    function seedColor(str) {
+        var hash = 0;
+        for (var i = 0; i < str.length; i++) {
+            hash = str.charCodeAt(i) + ((hash << 5) - hash);
+        }
+        var c = (hash & 0x00FFFFFF)
+            .toString(16)
+            .toUpperCase();
+
+        return "00000".substring(0, 6 - c.length) + c;
+    }
+
+    function host(uri) {
+        if (!uri) return $ls.stashURL;
+        if (uri.indexOf('http') === 0) return uri;
+
+        var splitRegExp = /\/(\b|$)/g;
+        return (_.compact(host().split(splitRegExp).concat(uri.split(splitRegExp)))).join('/');
+    }
+
+    function getAvatar(user) {
+        //return host(user.avatarUrl.split("?")[0]);
+        return host(user.avatarUrl);
+    }
+
+    function timeAgo(date) {
+        return moment(date).fromNow();
+    }
+
+    function vetoTooltip(pr) {
+        var blockers = pr.mergeStatus.vetoes.length == 1 ? ' Blocker for merge' : ' Blockers for merge';
+        var colon = pr.mergeStatus.vetoes.length == 0 ? '' : ':';
+        return [pr.mergeStatus.vetoes.length + ' ' + blockers + colon]
+            .concat(pr.mergeStatus.vetoes.map((veto) => {
+                return '- ' + veto.summaryMessage
+            }))
+            .join('\n');
+    }
+
+    function toTitleCase(str) {
+        return str.replace(/\w\S*/g, (txt) => {
+            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        });
+    }
+
+    function _breakCamelCase(str) {
+        return str.replace(/([A-Z])/g, ' $1').trim()
+    }
+
+    function commentTooltip(pr) {
+        return Object.keys(pr.properties).map((prop) => {
+            return toTitleCase(_breakCamelCase(prop)) + ': ' + pr.properties[prop];
+        }).join('\n');
+    }
+
+    function get(uri, cb) {
+
+        $ls.error = false;
+        delete $ls.errorMsg;
+
+        if (!_.isFunction(cb)) cb = _.noop;
+
+        return new Promise((resolve, reject) => {
+            $http({
+                method: 'GET',
+                url: host(uri)
+            }).then(function success(res) {
+                resolve(res.data);
+                cb(null, res.data);
+            }, function error(res) {
+                $ls.error = true;
+                $ls.errorMsg = res.status + ': ' + res.statusText;
+                cb(new Error(res.status));
+                reject(res.status);
+            });
+        });
+    }
+
+    /** Identity function */
+    const identity = (v) => v;
+
+    // Regex parts:  scheme  junk           host      path      query
+    const regex = /^(https?)(?:\:\/\/)?(\/?[^\/]+)(\/?.+?)(?:\?(.+))?$/;
+
+    function splitUrl(url) {
+        /** Split url into parts */
+        var [scheme, host, path, query] = url.match(regex).slice(1);
+
+        /** Path to array */
+        path = !path ? [] : path.split('/').map(decodeURIComponent).filter(identity);
+
+        /** Query to object */
+        query = !query ? [] : query.split('&').reduce((result, pair) => {
+            var [name, value] = pair.split('=');
+            result[decodeURIComponent(name)] = decodeURIComponent(value);
+            return result
+        }, {});
+
+        return {scheme, host, path, query};
+    }
+
+    function stashRemoteFromUrl(url) {
+        const {scheme, host, path} = splitUrl(url);
+        const result = {scheme, host};
+        const setResult = (key, arr, idx) => {
+            if (path[idx + 1]) {
+                result[key] = path[idx + 1];
+            }
+        };
+        if (path.length) {
+            let i;
+
+            path.forEach((node, idx, arr) => {
+                switch (node) {
+                    case 'projects':
+                        setResult('project', arr, idx);
+                        break;
+                    case 'repos':
+                        setResult('repo', arr, idx);
+                        break;
+                    case 'pull-requests':
+                        setResult('pr', arr, idx);
+                        break;
+                    case 'tasks':
+                        setResult('task', arr, idx);
+                        break;
+                    default:
+                        break;
+                }
+            });
+        }
+
+        return result;
+    }
+
+    const ex = {
+        seedColor: seedColor,
+        host: host,
+        getAvatar: getAvatar,
+        toTitleCase: toTitleCase,
+        timeAgo: timeAgo,
+        vetoTooltip: vetoTooltip,
+        commentTooltip: commentTooltip,
+        get: get,
+        splitUrl: splitUrl,
+        stashRemoteFromUrl: stashRemoteFromUrl,
+        uuid: uuid
+    };
+
+    return ex;
+
+
+}]);
