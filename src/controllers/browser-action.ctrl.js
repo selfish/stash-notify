f9e831db6a0c7eece1277d654e6d5dc6f6e52b4f
Nitai J. Perez <Nitai.Perez@Gmail.com>
2016-07-11 16:37:01 +0300

    Migrated PR filtering from query to view

--- src/controllers/browser-action.ctrl.js
+++ src/controllers/browser-action.ctrl.js
@@ -55,10 +55,45 @@ app.controller('prCtrl', ['$rootScope', '$scope', '$location', 'ls', 'util',
         };
 
         function updateView() {
-            $scope.prData = ls.get('prData');
+            debugger;
+            $scope.prData = ls.get('prData').filter(pr => {
+                // Scrum master mode:
+                if (ls.get('config').scrumMaster) {
+                    if (util.me()) {
+                        var hasMultipleReviewers = pr.reviewers.length > 1;
+                        var hasApproval = _.some(pr.reviewers, rev => {
+                            return (rev.approved === true && rev.user.name !== util.me());
+                        });
+
+                        if (hasMultipleReviewers && !hasApproval) {
+                            return false;
+                        }
+                    }
+                    // Show pull requests which were approved but not merged,
+                    // else if becuase if used with Scrum Master mode, will never show anything:
+                } else if (!ls.get('config').showApprovedPRs) {
+                    var hasApproval = _.some(pr.reviewers, rev => {
+                        return (rev.approved === true);
+                    });
+                    debugger;
+                    if (hasApproval) {
+                        return false;
+                    }
+                }
+
+                // TODO: Open tasks:
+                // if (ls.get('config').hidePrWithTasks) {
+                //     data.values = data.values.filter(pr => {
+                //         return _.get(pr, 'properties.openTaskCount') === 0;
+                //     });
+                // }
+
+                return true;
+            });
             $scope.prDataMine = ls.get('prDataMine');
         }
 
+        $rootScope.$on('updateView', updateView);
         updateView();
         // crosstab.on('ls', updateView);
     }]);
